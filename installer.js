0b9a2700ea5b660b977758c9cf77cd098994e8d0
initialencounter <109729945+initialencounter@users.noreply.github.com>
2023-01-15 14:14:02 +0800

    Update installer.js

--- installer.js
+++ installer.js
@@ -1,4 +1,4 @@
-const { spawn } = require('child_process')
+const spawn = require('child_process').spawn;
 const Yml2arr = /** @class */ (function () {
     // 构造函数 
     class Yml2arr {
@@ -28,12 +28,37 @@ const Yml2arr = /** @class */ (function () {
                         plugins_off.push(i);
                     });
                 } else {
+                    if(i.indexOf(':')==-1){
+                        i = i
+                    }else{
+                        i = i.slice(0,i.indexOf(':'))
+                    }
                     if (i.slice(0, 1) == '~') {
+
                         plugins_off.push(i.slice(1));
                     } else {
                         plugins_on.push(i);
                     }
                 }
+            });
+            return [plugins_on, plugins_off];
+        }
+    }
+    return Yml2arr;
+}());
+
+if (require.main === module) {
+    const ls = new Yml2arr("koishi.yml")
+    console.log(ls.plugins_on)
+    console.log(ls.plugins_off)
+    const pkg_arr = ls.plugins_on
+    pkg_arr.forEach((i) => {
+        var spawn_execution = spawn('npm', ["i", `koishi-plugin-${i}`]);
+        spawn_execution.stdout.on('data', function (data) {
+            console.log('stdout: ' + data);
+        });
+    })
+}
             });
             return [plugins_on, plugins_off];
         }
